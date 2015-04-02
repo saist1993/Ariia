999aacec520572e246b79a4e9a652ec7bc629578
Divyaraj Masani <masanidivyaraj@gmail.com>
2015-04-02 16:18:11 +0530

    Updated Version

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
+//include header in html
+
 function clear() {
-			$("#doctor").trigger('reset');
+			$("#").trigger('reset');
 			document.getElementById('choice').disabled=true;
 			document.getElementById('userId').value="";
 }
@@ -65,3 +67,76 @@ function setVisibility(id,visibility) {
 			el1.style.display='none';		
         el.style.display = visibility;
 	}
+
+//forget modal script
+function showPassword() {
+    
+    var key_attr = $('#key').attr('type');
+    
+    if(key_attr != 'text') {
+        
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+        
+    } else {
+        
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+        
+    }
+    
+}
+/*
+//Login Modal Script
+$(document).ready(function(){
+    $('.modal-footer button').click(function(){
+		var button = $(this);
+
+		if ( button.attr("data-dismiss") != "modal" ){
+			var inputs = $('form input');
+			var title = $('.modal-title');
+			var progress = $('.progress');
+			var progressBar = $('.progress-bar');
+
+			inputs.attr("disabled", "disabled");
+
+			button.hide();
+
+			progress.show();
+
+			progressBar.animate({width : "100%"}, 100);
+
+			progress.delay(1000)
+					.fadeOut(600);
+
+			button.text("Close")
+					.removeClass("btn-primary")
+					.addClass("btn-success")
+    				.blur()
+					.delay(1600)
+					.fadeIn(function(){
+						title.text("Log in is successful");
+						button.attr("data-dismiss", "modal");
+					});
+		}
+	});
+
+	$('#myModal').on('hidden.bs.modal', function (e) {
+		var inputs = $('form input');
+		var title = $('.modal-title');
+		var progressBar = $('.progress-bar');
+		var button = $('.modal-footer button');
+
+		inputs.removeAttr("disabled");
+
+		title.text("Log in");
+
+		progressBar.css({ "width" : "0%" });
+
+		button.removeClass("btn-success")
+				.addClass("btn-primary")
+				.text("Ok")
+				.removeAttr("data-dismiss");
+                
+	});
+});*/
